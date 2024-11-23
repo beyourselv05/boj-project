4615349f95bd2c2b91af1914df4d6c24142b4c28
beyourselv05 <beyourselv05@gmail.com>
2024-11-23 15:30:54 +0900

    Add files via upload

---
+++ 소트인사이드.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h> //정수 말고 문자열로 접근
+
+int main() {
+
+	char arr[11];	// 입력받을 문자열 저장
+	int i, j;
+
+	scanf("%s", arr);
+
+	// 정렬 구현
+	for (i = 9; i >= 0; i--) {
+		for (j = 0; j < strlen(arr); j++) {
+			if (arr[j] == i + 48) {	// 아스키코드 0=48 
+				printf("%c", arr[j]);
+			}
+		}
+	}
+}
